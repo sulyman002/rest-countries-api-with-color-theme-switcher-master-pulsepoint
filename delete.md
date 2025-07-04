ed8d9443b5299806d25135a88a49bc06398a5611
sulyman002 <oyedelesulaiman@gmail.com>
2025-07-04 15:17:51 +0100

    working on js

---
+++ delete.md
@@ -0,0 +1,18 @@
+
+        // const filterRegion = [...new set(data.map(dRegion => dRegion.region))];
+
+        // filterRegion.forEach(regions => {
+        //     const regionOptiion = document.createElement('option');
+        //     regionOptiion.value = regions;
+        //     regionOptiion.textContent = regions;
+        //     selectRegion.appendChild(regionOptiion);
+        // });
+
+        // console.log(data);
+
+        //      regions.forEach(region => {
+        //   const option = document.createElement('option');
+        //   option.value = region;
+        //   option.textContent = region;
+        //   regionSelect.appendChild(option);
+        // });
--- script.js
+++ script.js
@@ -4,36 +4,19 @@ fetch('data.json')
 
         const selectRegion = document.getElementById('selectRegion');
 
-        // const filterRegion = [...new set(data.map(dRegion => dRegion.region))];
 
-        // filterRegion.forEach(regions => {
-        //     const regionOptiion = document.createElement('option');
-        //     regionOptiion.value = regions;
-        //     regionOptiion.textContent = regions;
-        //     selectRegion.appendChild(regionOptiion);
-        // });
-
-        // console.log(data);
-
-        //      regions.forEach(region => {
-        //   const option = document.createElement('option');
-        //   option.value = region;
-        //   option.textContent = region;
-        //   regionSelect.appendChild(option);
-        // });
-
-            data.forEach((dataV, index) => {
+        data.forEach((dataV, index) => {
 
 
-                selectRegion.innerHTML += `<option value="${dataV.region}">${dataV.region}</option>`;
+            selectRegion.innerHTML += `<option value="${dataV.region}">${dataV.region}</option>`;
 
-            })
+        })
 
         const countryCardsContainer = document.querySelector('.countryCardsContainer');
 
         data.forEach(country => {
             const cardTemplate = document.createElement('div');
-            cardTemplate.className = 'flex hidden flex-col w-full bg-[#2b3945] shadow-lg rounded-md';
+            cardTemplate.className = 'flex hidde flex-col w-full bg-[#2b3945] shadow-lg rounded-md';
 
             cardTemplate.innerHTML += `
                 <div>
@@ -45,36 +28,74 @@ fetch('data.json')
                     <p class="text-white font-medium">Region: <span class="font-thin text-gray-400"> ${" " + country.region}</span></p>
                     <p class="text-white font-medium">Captal:<span class="font-thin text-gray-400"> ${" " + country.capital}</p>
                 </div>
-            `;countryCardsContainer.appendChild(cardTemplate);
+            `; countryCardsContainer.appendChild(cardTemplate);
 
         });
 
 
         const searchInput = document.querySelector('.searchInput');
         const searchBtn = document.querySelector('.searchBtn');
-        //countryCardsContainer ~ AREA TO DISPLAY
+        const showDetailsOfCountries = document.querySelector('.showDetailsOfCountries');
 
 
         function displaySearchedCountries(countriesSearch) {
-            countryCardsContainer.innerHTML = "";
-            countriesSearch.forEach(country =>{
-                const counteyElement = document.createElement('div');
-                counteyElement.textContent = country.name;
-                countryCardsContainer.appendChild(counteyElement);
+            showDetailsOfCountries.innerHTML = "";
+            countriesSearch.forEach(country => {
+                const countryElement = document.createElement('div');
+                countryElement.className = '';
+                counteyElement.innerHTML = `
+                    <div class="mt-14 hiddn flex flex-col md:flex-row md:gap-44">
+                        <div class="md:w-1/2 bg-red-90 shadow-2xl"><img class="md:w-5/5 md:h-full"
+                                src="https://flagcdn.com/af.svg" alt="" height="40px"></div>
+                        <div class="md:w-1/2">
+                            <h1 class="text-white mt-10 font-bold text-2xl cursor-pointer">Belgium</h1>
+                            <div class="md:flex w-full gap-60">
+
+                                <div class="text-white mt-6 md:mt-8  leading-loose">
+                                    <p>Native Name:${" " + country.nativeName}</p>
+                                    <p>Population:${" " + country.population}</p>
+                                    <p>Region:${" " + country.region}</p>
+                                    <p>Sub Region:${" " + country.subregion}</p>
+                                    <p>Capital:${" " + country.capital}</p>
+                                </div>
+                                <div class="text-white mt-6 md:mt-8 leading-loose">
+                                    <p>Top Level Domain:${" " + country.topLevelDomain}</p>
+                                    <p>Currencies:${" " + country.currencies[0].name}</p>
+                                    <p>Languages:${" " + country.languages[0].name}</p>
+                                </div>
+
+                            </div>
+
+                            <div class="text-white mt-6 md:mt-24 md:flex md:items-center md:justify-start md:gap-6">
+                                <p class="text-1xl font-semibold mb-4">Border Countries:</p>
+                                <div class="flex gap-4 w-full md:w-2/4">
+                                    <button
+                                        class="px-6 py-1 bg-[#2b3945] flex text-white items-center justify-center shadow-2xl p-1 gap-1 rounded-sm cursor-pointer">France</button>
+                                    <button
+                                        class="px-6 py-1 bg-[#2b3945] flex text-white items-center justify-center shadow-2xl p-1 gap-1 rounded-sm cursor-pointer">Germany</button>
+                                    <button
+                                        class="px-6 py-1 bg-[#2b3945] flex text-white items-center justify-center shadow-2xl p-1 gap-1 rounded-sm cursor-pointer">Netherlands</button>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                `;
+                showDetailsOfCountries.appendChild(countryElement);
             });
         }
 
-        displaySearchedCountries(data.name);
+        displaySearchedCountries(data);
 
         searchBtn.addEventListener('click', () => {
             const search = searchInput.value.trim().toLowerCase();
 
             if (search) {
-                const filterCountry = data.filter(countrys => 
-                    countrys.namee.toLowerCase().include(search)
+                const filterCountry = data.filter(country =>
+                    country.name.toLowerCase().includes(search)
                 );
                 displaySearchedCountries(filterCountry);
-            }else {
+            } else {
                 displaySearchedCountries(data.name);
             }
         }); // check it out tomorrow.
